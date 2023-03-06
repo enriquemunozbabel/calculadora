a803cfbcfcaffe65db4e4bf35391e45b4a04fc80
enriquemunozbabel <125457875+enriquemunozbabel@users.noreply.github.com>
2023-03-06 11:48:08 +0100

    Initial commit

---
+++ src/main/java/com/hospital/citasHospital/CitasHospitalApplication.java
@@ -0,0 +1,14 @@
+package com.hospital.citasHospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitasHospitalApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitasHospitalApplication.class, args);
+	}
+
+}
