be79e998180e0cc15c383c94fb60c919f025139a
enriquemunozbabel <125457875+enriquemunozbabel@users.noreply.github.com>
2023-03-07 11:24:58 +0100

    test

--- src/test/java/com/calculadora/CalculadoraApplicationTests.java
+++ src/test/java/com/calculadora/CalculadoraApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CitasHospitalApplicationTests {
+class CalculadoraApplicationTests {
 
 	@Test
 	void contextLoads() {
